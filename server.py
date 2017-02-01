65f5c6f1daee63727139e30a4f256cb2d6bd5b86
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-01 12:09:56 -0800

    registration of users implemented

--- server.py
+++ server.py
@@ -2,10 +2,10 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask, jsonify
+from flask import Flask, render_template, redirect, request, flash, session, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from model import User, Rating, Movie, connect_to_db, db
 
 
 app = Flask(__name__)
@@ -22,8 +22,40 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def index():
     """Homepage."""
-    a = jsonify([1,3])
-    return a
+
+    return render_template("homepage.html")
+
+@app.route("/users")
+def user_list():
+    """Show list of users."""
+
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
+
+@app.route("/register", methods=["GET"])
+def register_form():
+
+    return render_template("register_form.html")
+
+@app.route("/register", methods=["POST"])
+def register_process():
+    email = request.form.get("email")
+    password = request.form.get("password")
+    age = request.form.get("age")
+    zipcode = request.form.get("zipcode")
+
+    if User.query.filter_by(email=email).first() is None:
+        user = User(email=email,
+                    password=password,
+                    age=age,
+                    zipcode=zipcode)
+        db.session.add(user)
+        db.session.commit()
+        print "You are now registered!"
+        return "You are now registered! yay!"
+    else:
+        print "Username taken."
+        return "Username taken."
 
 
 if __name__ == "__main__":
